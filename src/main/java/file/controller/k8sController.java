5b18cd5feb1182b9a2e9440a176b6116f2b165f2
hyesongChae <chs5264@naver.com>
2025-02-03 16:24:18 +0900

    update controller5

--- src/main/java/file/controller/k8sController.java
+++ src/main/java/file/controller/k8sController.java
@@ -15,4 +15,9 @@ public class k8sController {
 	public String GetStatus2 () {
 		return "Status - returned by Pod" ;
 	}
+	
+	@GetMapping(value = "/api/status3")
+	public String GetStatus3 () {
+		return "Status - returned by Pod" ;
+	}
 }
